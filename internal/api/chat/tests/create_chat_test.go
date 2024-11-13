908ccac5e42bd49b45212be928e89ecd3484be5f
Pavel <psjob@bk.ru>
2024-11-13 12:09:28 +0300

    Переделал тесты

--- internal/api/chat/tests/create_chat_test.go
+++ internal/api/chat/tests/create_chat_test.go
@@ -2,103 +2,69 @@ package tests
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"testing"
 
 	"github.com/brianvoe/gofakeit"
 	"github.com/gojuno/minimock/v3"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/spv-dev/chat-server/internal/api/chat"
 	"github.com/spv-dev/chat-server/internal/model"
-	"github.com/spv-dev/chat-server/internal/service"
 	serviceMocks "github.com/spv-dev/chat-server/internal/service/mocks"
 	desc "github.com/spv-dev/chat-server/pkg/chat_v1"
 )
 
 func TestCreateChat(t *testing.T) {
 	t.Parallel()
-	type chatServiceMockFunc func(mc *minimock.Controller) service.ChatService
 
-	type args struct {
-		ctx context.Context
-		req *desc.CreateChatRequest
-	}
+	ctx := context.Background()
 
-	var (
-		ctx = context.Background()
-		mc  = minimock.NewController(t)
+	chatID := gofakeit.Int64()
+	userIDs := []int64{gofakeit.Int64(), gofakeit.Int64(), gofakeit.Int64()}
+	title := gofakeit.JobTitle()
 
-		chatID  = gofakeit.Int64()
-		userIDs = []int64{gofakeit.Int64(), gofakeit.Int64(), gofakeit.Int64()}
-		title   = gofakeit.JobTitle()
-
-		serviceErr = fmt.Errorf("service error")
-
-		req = &desc.CreateChatRequest{
-			Info: &desc.ChatInfo{
-				Title:   title,
-				UserIds: userIDs,
-			},
-		}
-
-		resp = &desc.CreateChatResponse{
-			Id: chatID,
-		}
-
-		chatInfo = &model.ChatInfo{
+	req := &desc.CreateChatRequest{
+		Info: &desc.ChatInfo{
 			Title:   title,
-			UserIDs: userIDs,
-		}
-	)
-
-	tests := []struct {
-		name            string
-		args            args
-		want            *desc.CreateChatResponse
-		err             error
-		chatServiceMock chatServiceMockFunc
-	}{
-		{
-			name: "Success Create Chat",
-			args: args{
-				ctx: ctx,
-				req: req,
-			},
-			want: resp,
-			err:  nil,
-			chatServiceMock: func(mc *minimock.Controller) service.ChatService {
-				mock := serviceMocks.NewChatServiceMock(mc)
-				mock.CreateChatMock.Expect(ctx, chatInfo).Return(chatID, nil)
-				return mock
-			},
-		},
-		{
-			name: "Error Create Chat",
-			args: args{
-				ctx: ctx,
-				req: req,
-			},
-			want: nil,
-			err:  serviceErr,
-			chatServiceMock: func(mc *minimock.Controller) service.ChatService {
-				mock := serviceMocks.NewChatServiceMock(mc)
-				mock.CreateChatMock.Expect(ctx, chatInfo).Return(0, serviceErr)
-				return mock
-			},
+			UserIds: userIDs,
 		},
 	}
 
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			chatServiceMock := tt.chatServiceMock(mc)
-			api := chat.NewServer(chatServiceMock)
-
-			res, err := api.CreateChat(tt.args.ctx, tt.args.req)
-			require.Equal(t, tt.err, err)
-			require.Equal(t, tt.want, res)
-		})
+	resp := &desc.CreateChatResponse{
+		Id: chatID,
 	}
+
+	chatInfo := &model.ChatInfo{
+		Title:   title,
+		UserIDs: userIDs,
+	}
+
+	mc := minimock.NewController(t)
+
+	service := serviceMocks.NewChatServiceMock(mc)
+
+	api := chat.NewServer(service)
+
+	t.Run("create chat success", func(t *testing.T) {
+		t.Parallel()
+
+		service.CreateChatMock.Expect(ctx, chatInfo).Return(chatID, nil)
+
+		r, err := api.CreateChat(ctx, req)
+
+		assert.NoError(t, err)
+		assert.Equal(t, r, resp)
+	})
+
+	serviceErr := errors.New("service error")
+	t.Run("create chat error", func(t *testing.T) {
+		t.Parallel()
+
+		service.CreateChatMock.Expect(ctx, chatInfo).Return(0, serviceErr)
+
+		_, err := api.CreateChat(ctx, req)
+
+		assert.Equal(t, err, serviceErr)
+	})
 }
--- internal/api/chat/tests/send_message_test.go
+++ internal/api/chat/tests/send_message_test.go
@@ -2,102 +2,66 @@ package tests
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"testing"
 
 	"github.com/brianvoe/gofakeit"
 	"github.com/gojuno/minimock/v3"
-	"github.com/stretchr/testify/require"
-	"google.golang.org/protobuf/types/known/emptypb"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/spv-dev/chat-server/internal/api/chat"
 	"github.com/spv-dev/chat-server/internal/model"
-	"github.com/spv-dev/chat-server/internal/service"
 	serviceMocks "github.com/spv-dev/chat-server/internal/service/mocks"
 	desc "github.com/spv-dev/chat-server/pkg/chat_v1"
 )
 
 func TestSendMessage(t *testing.T) {
 	t.Parallel()
-	type chatServiceMockFunc func(mc *minimock.Controller) service.ChatService
 
-	type args struct {
-		ctx context.Context
-		req *desc.SendMessageRequest
-	}
+	ctx := context.Background()
 
-	var (
-		ctx = context.Background()
-		mc  = minimock.NewController(t)
+	chatID := gofakeit.Int64()
+	userID := gofakeit.Int64()
+	text := gofakeit.Company()
 
-		chatID = gofakeit.Int64()
-		userID = gofakeit.Int64()
-		text   = gofakeit.Company()
-
-		serviceErr = fmt.Errorf("service error")
-
-		req = &desc.SendMessageRequest{
-			Info: &desc.MessageInfo{
-				Body:   text,
-				ChatId: chatID,
-				UserId: userID,
-			},
-		}
-
-		message = &model.MessageInfo{
+	req := &desc.SendMessageRequest{
+		Info: &desc.MessageInfo{
 			Body:   text,
-			ChatID: chatID,
-			UserID: userID,
-		}
-	)
-
-	tests := []struct {
-		name            string
-		args            args
-		want            *emptypb.Empty
-		err             error
-		chatServiceMock chatServiceMockFunc
-	}{
-		{
-			name: "Success Send Message",
-			args: args{
-				ctx: ctx,
-				req: req,
-			},
-			want: nil,
-			err:  nil,
-			chatServiceMock: func(mc *minimock.Controller) service.ChatService {
-				mock := serviceMocks.NewChatServiceMock(mc)
-				mock.SendMessageMock.Expect(ctx, message).Return(nil)
-				return mock
-			},
-		},
-		{
-			name: "Error Send Message",
-			args: args{
-				ctx: ctx,
-				req: req,
-			},
-			want: nil,
-			err:  serviceErr,
-			chatServiceMock: func(mc *minimock.Controller) service.ChatService {
-				mock := serviceMocks.NewChatServiceMock(mc)
-				mock.SendMessageMock.Expect(ctx, message).Return(serviceErr)
-				return mock
-			},
+			ChatId: chatID,
+			UserId: userID,
 		},
 	}
 
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-			chatServiceMock := tt.chatServiceMock(mc)
-			api := chat.NewServer(chatServiceMock)
-
-			res, err := api.SendMessage(tt.args.ctx, tt.args.req)
-			require.Equal(t, tt.err, err)
-			require.Equal(t, tt.want, res)
-		})
+	message := &model.MessageInfo{
+		Body:   text,
+		ChatID: chatID,
+		UserID: userID,
 	}
+
+	mc := minimock.NewController(t)
+
+	service := serviceMocks.NewChatServiceMock(mc)
+
+	api := chat.NewServer(service)
+
+	t.Run("send message success", func(t *testing.T) {
+		t.Parallel()
+
+		service.SendMessageMock.Expect(ctx, message).Return(nil)
+
+		_, err := api.SendMessage(ctx, req)
+
+		assert.NoError(t, err)
+	})
+
+	serviceErr := errors.New("service error")
+	t.Run("send message error", func(t *testing.T) {
+		t.Parallel()
+
+		service.SendMessageMock.Expect(ctx, message).Return(serviceErr)
+
+		_, err := api.SendMessage(ctx, req)
+
+		assert.Equal(t, err, serviceErr)
+	})
 }
